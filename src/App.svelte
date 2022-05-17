79cfbcf19086160d3981c009e17ce36bbfd1a5e8
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-17 18:44:22 +0500

    Add component wrapper

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<div class="component-wrapper" />
+
+<style lang="scss">
+  .component-wrapper {
+    width: 100%;
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+  }
+</style>
