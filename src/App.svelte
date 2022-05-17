717a97b8d7a8da035b440f2680ba20822b1c8140
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-17 19:59:40 +0500

    Make DashboardCard skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,11 @@
-<div class="component-wrapper" />
+<script lang="ts">
+  import DashboardCard from "./lib/atomic/DashboardCard.svelte";
+  import data from "./data/data.json";
+</script>
+
+<div class="component-wrapper">
+  <DashboardCard activity={data[0]} timeframe="daily" />
+</div>
 
 <style lang="scss">
   .component-wrapper {
---
+++ src/lib/atomic/DashboardCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Activity, Timeframe } from "$src/types";
+  import { getPreviousDurationText } from "$src/utils";
+
+  export let activity: Activity;
+
+  /** The current selected timeframe */
+  export let timeframe: Timeframe;
+
+  $: previousDuration = getPreviousDurationText(timeframe);
+</script>
+
+<article class="card">
+  <h2 class="card__title">
+    {activity.title}
+  </h2>
+  <strong class="card__current-duration">
+    <time>{activity.timeframes[timeframe].current}hrs</time>
+  </strong>
+  <p class="card__previous-duration">
+    {previousDuration} &mdash;
+    <time>{activity.timeframes[timeframe].previous}hrs</time>
+  </p>
+</article>
