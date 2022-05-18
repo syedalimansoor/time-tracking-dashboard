8e7d61abe8a0efee0c68a6a418a1c4a27c4e5d9e
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-18 20:05:57 +0500

    Convert development to top-down

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
-  import { setContext } from "svelte";
-  import type { Writable } from "svelte/store";
-  import { writable } from "svelte/store";
+  import Dashboard from "./lib/composition/Dashboard.svelte";
   import activities from "./data/data.json";
-  import DashboardCardGrid from "./lib/composition/DashboardCardGrid.svelte";
+
   import "./style/global.scss";
-  import type { Timeframe } from "./types";
-  const timeframe: Writable<Timeframe> = writable<Timeframe>("daily");
-  setContext("timeframe", timeframe);
 </script>
 
-<div class="component-wrapper">
-  <DashboardCardGrid {activities} />
+<div class="wrapper">
+  <div class="dashboard-wrapper">
+    <Dashboard {activities} />
+  </div>
 </div>
 
 <style lang="scss">
-  .component-wrapper {
-    width: 100%;
+  .wrapper {
     min-height: 100vh;
+    background-color: $clr-very-dark-blue;
+
     display: grid;
     place-items: center;
+  }
+
+  .dashboard-wrapper {
+    width: 100%;
+    height: 100%;
+    padding: {
+      block: 4em;
+      inline: 1.5em;
+    }
   }
 </style>
---
+++ src/lib/composition/Dashboard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+  import type { Writable } from "svelte/store";
+  import { writable } from "svelte/store";
+  import DashboardCardGrid from "$src/lib/composition/DashboardCardGrid.svelte";
+  import type { Activity, Timeframe } from "$src/types";
+
+  export let activities: Activity[];
+
+  const timeframe: Writable<Timeframe> = writable<Timeframe>("daily");
+  setContext("timeframe", timeframe);
+</script>
+
+<main>
+  <DashboardCardGrid {activities} />
+</main>
