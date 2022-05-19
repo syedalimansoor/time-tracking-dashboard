e5e2a68c778a4c8355b64ab1e3dd72e4edf5f5e3
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-05-19 22:38:50 +0500

    Style TimeframeSelector for mobile

---
+++ src/lib/atomic/TimeframeSelector.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import type { Timeframe } from "$src/types";
+
+  import { getContext } from "svelte";
+  import type { Writable } from "svelte/store";
+
+  const timeframe: Writable<Timeframe> = getContext("timeframe");
+</script>
+
+<fieldset class="selector">
+  <label>
+    <input
+      class="selector__optn"
+      type="radio"
+      name="timeframe"
+      bind:group={$timeframe}
+      value="daily"
+    />
+    <span class="selector__label">Daily</span>
+  </label>
+  <label>
+    <input
+      class="selector__optn"
+      type="radio"
+      name="timeframe"
+      bind:group={$timeframe}
+      value="weekly"
+    />
+    <span class="selector__label">Weekly</span>
+  </label>
+  <label>
+    <input
+      class="selector__optn"
+      type="radio"
+      name="timeframe"
+      bind:group={$timeframe}
+      value="monthly"
+    />
+    <span class="selector__label">Monthly</span>
+  </label>
+</fieldset>
+
+<style lang="scss">
+  .selector {
+    border: none;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    padding-block: 1.2em;
+
+    &__label {
+      font-weight: $fw-light;
+      color: $clr-desaturated-blue;
+    }
+
+    &__optn {
+      width: 0;
+      height: 0;
+      opacity: 0;
+
+      &:checked ~ .selector__label {
+        color: white;
+      }
+    }
+  }
+</style>
